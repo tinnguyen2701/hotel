e56800ae9ab7c62400d90c2680567d3624f96725
Tin Dinh. Nguyen <tin.nguyen@vServer.com>
2020-06-15 13:09:43 +0700

    buoi 8

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,11 +4,10 @@ import {RouterModule} from '@angular/router';
 //
 import {ThemeModule} from '@app/theme';
 import {GuestGuard} from '@app/modules/auth/services';
-import {AuthComponent} from '@app/modules/auth/components/auth.component';
 import {LoginFormComponent} from '@app/modules/auth/components/login-form/login-form.component';
 
 const COMPONENTS = [
-  AuthComponent, LoginFormComponent
+  LoginFormComponent
 ];
 
 
